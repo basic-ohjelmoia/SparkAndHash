a561cb666bfc8ab9806717a4dc8eebb823026375
Tuomas H <tuho@cs.helsinki.fi>
2018-10-24 04:41:31 +0300

    Fixed the executable

--- src/main/java/com/acme/sparkandhash/SparkAndHash.java
+++ src/main/java/com/acme/sparkandhash/SparkAndHash.java
@@ -35,7 +35,7 @@ public class SparkAndHash {
     }
 
 
-    public static void main(String[] args) {
+ public static void main(String[] args) { 
 
         get("/", (req, res) -> "Hello world!");
 
